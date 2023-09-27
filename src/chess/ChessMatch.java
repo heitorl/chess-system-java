d8b9fe76e58d1103a7be9f86b6e8ef7ee1460467
heitor lourenco <juniorio01@hotmail.com>
2023-09-27 20:58:20 -0300

    feat: possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,6 +34,9 @@ public class ChessMatch {
     if(!board.thereIsApiece(position)){
       throw new ChessException("There is no piece on source position.");
     }
+    if(!board.piece(position).isThereAnyPossibleMove()){
+      throw new ChessException("There is no possible moves for the chosen piece.");
+    }
   }
 
   public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
